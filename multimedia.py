4f941019b58e6bcb3152755a8ba98f6d941616a4
makmur21 <136968703+makmur21@users.noreply.github.com>
2024-08-08 01:01:42 +0000

    chapter 1

---
+++ multimedia.py
@@ -0,0 +1,10 @@
+from tracemalloc import BaseFilter
+import pygame
+import PIL
+import cv2
+import moviepy.editor as mp
+import pydub
+import tkinter as tk
+
+print("✅ Semua pustaka telah terinstal dengan benar!")
+
